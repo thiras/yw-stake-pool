78aad1532f9d341c4a5970fb4c91b819efd6d0f2
Ant Somers <antsomers@gmail.com>
2025-10-19 03:05:40 +0300

    fix(scripts): change lintArgs to let and filter out unsupported file paths

--- scripts/program/lint.mjs
+++ scripts/program/lint.mjs
@@ -10,10 +10,14 @@ import {
 
 // Configure additional arguments here, e.g.:
 // ['--arg1', '--arg2', ...cliArguments()]
-const lintArgs = cliArguments();
+let lintArgs = cliArguments();
 
 const fix = popArgument(lintArgs, '--fix');
 const toolchain = getToolchainArgument('lint');
+
+// Filter out file paths that may be passed by lint-staged
+// cargo clippy doesn't accept individual file paths
+lintArgs = lintArgs.filter((arg) => !arg.endsWith('.rs') && !arg.includes('/'));
 
 // Lint the programs using clippy.
 for (const folder of getProgramFolders()) {
