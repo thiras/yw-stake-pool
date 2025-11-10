5d7970df5f4fbfbc05f4c92ecf857dc49fea5568
Ant Somers <antsomers@gmail.com>
2025-11-11 02:06:26 +0300

    chore: remove unused imports and variables in M-01 test
    - Remove unused BorshSerialize import
    - Remove unused AccountMeta and Instruction imports
    - Remove unused StakePoolInstruction and StakePool imports
    - Remove unused reward_mint variable
    
    This cleanup eliminates compiler warnings while maintaining test functionality.

--- program/tests/m01_frontrun_dos_tests.rs
+++ program/tests/m01_frontrun_dos_tests.rs
@@ -4,16 +4,13 @@
 
 mod common;
 
-use borsh::BorshSerialize;
 use litesvm::LiteSVM;
 use solana_sdk::{
-    instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
     signature::{Keypair, Signer},
     system_instruction,
     transaction::Transaction,
 };
-use your_wallet_stake_pool::{instruction::StakePoolInstruction, state::StakePool};
 
 use common::*;
 
@@ -29,7 +26,6 @@ fn test_m01_frontrunning_dos_resistance() {
     svm.airdrop(&authority.pubkey(), 10_000_000_000).unwrap();
 
     let stake_mint = Keypair::new().pubkey();
-    let reward_mint = Keypair::new().pubkey();
     let pool_id = 0u64;
     let (pool_pda, _) = get_pool_pda(&authority.pubkey(), &stake_mint, pool_id);
 
