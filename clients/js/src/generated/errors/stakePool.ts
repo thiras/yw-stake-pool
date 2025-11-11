9cb2beb0537511cbdbec9dafcdbd2eb586c2b0e2
Ant Somers <antsomers@gmail.com>
2025-11-11 03:42:46 +0300

    chore(client): add unexpected balance change error for token transfers

--- clients/js/src/generated/errors/stakePool.ts
+++ clients/js/src/generated/errors/stakePool.ts
@@ -68,6 +68,8 @@ export const STAKE_POOL_ERROR__INVALID_PARAMETERS = 0x18; // 24
 export const STAKE_POOL_ERROR__INVALID_VAULT_OWNER = 0x19; // 25
 /** UnsafeTokenExtension: Unsafe Token-2022 extension detected */
 export const STAKE_POOL_ERROR__UNSAFE_TOKEN_EXTENSION = 0x1a; // 26
+/** UnexpectedBalanceChange: Unexpected token balance change during transfer */
+export const STAKE_POOL_ERROR__UNEXPECTED_BALANCE_CHANGE = 0x1b; // 27
 
 export type StakePoolError =
   | typeof STAKE_POOL_ERROR__ACCOUNT_MISMATCH
@@ -96,6 +98,7 @@ export type StakePoolError =
   | typeof STAKE_POOL_ERROR__POOL_PAUSED
   | typeof STAKE_POOL_ERROR__SERIALIZATION_ERROR
   | typeof STAKE_POOL_ERROR__UNAUTHORIZED
+  | typeof STAKE_POOL_ERROR__UNEXPECTED_BALANCE_CHANGE
   | typeof STAKE_POOL_ERROR__UNSAFE_TOKEN_EXTENSION;
 
 let stakePoolErrorMessages: Record<StakePoolError, string> | undefined;
@@ -127,6 +130,7 @@ if (process.env.NODE_ENV !== 'production') {
     [STAKE_POOL_ERROR__POOL_PAUSED]: `Pool is paused`,
     [STAKE_POOL_ERROR__SERIALIZATION_ERROR]: `Error serializing an account`,
     [STAKE_POOL_ERROR__UNAUTHORIZED]: `Unauthorized`,
+    [STAKE_POOL_ERROR__UNEXPECTED_BALANCE_CHANGE]: `Unexpected token balance change during transfer`,
     [STAKE_POOL_ERROR__UNSAFE_TOKEN_EXTENSION]: `Unsafe Token-2022 extension detected`,
   };
 }
